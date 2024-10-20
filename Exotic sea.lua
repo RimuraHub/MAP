81692aedcbfef6dd0f53c11d9b21cc13fde6a561
RimuraHub <lnwzaano@gmail.com>
2024-10-20 09:35:05 +0700

    Update Exotic sea.lua

--- Exotic sea.lua
+++ Exotic sea.lua
@@ -264,7 +264,6 @@ getgenv().Config = {
     ["Instant Kill"] = Monee,
     ["Radius"] = 15
 }
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Godmey/Inkillallmap/main/inkill.lua'))()
     end
 })
 T5:AddSection({"| Skill"})
@@ -294,6 +293,42 @@ end})
 
 ------[[ Spawn function ]]------
 
+spawn(function()
+    while wait() do
+        pcall(function()
+            if getgenv().Config["Instant Kill"] then
+                local player = game.Players.LocalPlayer
+                local character = player.Character or player.CharacterAdded:Wait()
+                sethiddenproperty(player, "SimulationRadius", 112412400000)
+                sethiddenproperty(player, "MaxSimulationRadius", 112412400000)
+                
+                local Den
+                if getgenv().Config["Folder Mon"] == "nil" then
+                    Den = game.Workspace:GetDescendants()
+                else
+                    Den = game.Workspace[getgenv().Config["Folder Mon"]]:GetDescendants()
+                end
+                
+                for _, v in pairs(Den) do
+                    if v:IsA("Humanoid") and v.Parent and v.Parent:IsA("Model") then
+                        -- ตรวจสอบว่าตัวละครไม่ใช่ผู้เล่น
+                        if v.Parent.Name ~= player.Name then
+                            local MonPoz = v.Parent:FindFirstChild("HumanoidRootPart") and v.Parent.HumanoidRootPart.Position
+                            local PlayerPoz = character:FindFirstChild("HumanoidRootPart") and character.HumanoidRootPart.Position
+                            
+                            if MonPoz and PlayerPoz and (MonPoz - PlayerPoz).Magnitude <= getgenv().Config["Radius"] then
+                                if v.Health < v.MaxHealth then
+                                    wait(.1)
+                                    v.Health = 0
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end)
+    end
+end)
 spawn(function()
     while true do
         task.wait()
