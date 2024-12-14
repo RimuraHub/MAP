0029cc090576cd47de8a44342fed6d89d31b3db7
cayvaii <condipiece99@gmail.com>
2024-12-14 16:32:57 +0700

    Update Muden.lua

--- Muden.lua
+++ Muden.lua
@@ -24,22 +24,22 @@ local function AddDropdownn(q,w,e,r,t,y)
 q:AddDropdown({Name=w,Options=e,Default=r,Flag=t,Callback=y})
 end
 function _Attack()
-game:GetService('VirtualUser'):CaptureController()
-game:GetService('VirtualUser'):ClickButton1(Vector2.new(851, 158),game:GetService("Workspace").Camera.CFrame)
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton1(Vector2.new(851, 158), game:GetService("Workspace").Camera.CFrame)
 end
 function EquipTool()
     for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-    if v:IsA("Tool") and v.Name == _G.Weapon then
-        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        if v:IsA("Tool") and v.Name == _G.Weapon then
+            game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        end
     end
-  end
 end
-function EquipToolSele(NameWaepon)
+function EquipToolSele(NameWeapon)
     for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-    if v:IsA("Tool") and v.Name == NameWaepon then
-        game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        if v:IsA("Tool") and v.Name == NameWeapon then
+            game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+        end
     end
-  end
 end
 function TP(cframe)
     pcall(function()
@@ -48,6 +48,26 @@ function TP(cframe)
             character:SetPrimaryPartCFrame(cframe)
         end
     end)
+end
+function sendNotification(title, text, duration)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = title,
+        Text = text,
+        Duration = duration
+    })
+end
+function fpp()
+    for _, v in pairs(game.Workspace:GetDescendants()) do
+        if v:IsA("ProximityPrompt") then
+            fireproximityprompt(v)
+        end
+    end
+end
+local function hasTool(container)
+    if container:FindFirstChild("SukunaV2") then
+        return true
+    end
+    return false
 end
 function Up(a,b)
 game:GetService("ReplicatedStorage").RemoteEvents.UpPoint:FireServer(a,b)
@@ -128,10 +148,24 @@ T2:AddToggle({
     end
 })
 T2:AddToggle({
-    Name = "Farm Asta[Have SukunaV2]",
+    Name = "Farm Asta",
     Default = false,
     Callback = function(AstaX)
-      _G.X = AstaX
+      _gv.X = AstaX
+    end
+})
+T2:AddToggle({
+    Name = "Farm Femto",
+    Default = false,
+    Callback = function(Femto)
+      _gv.AFemto = Femto
+    end
+})
+T2:AddToggle({
+    Name = "Farm Femto V2",
+    Default = false,
+    Callback = function(FemtoV2)
+      _gv.AFemtoV2 = FemtoV2
     end
 })
 T2:AddSection({"| Random"})
@@ -310,13 +344,177 @@ spawn(function()
     while true do
         task.wait()
         pcall(function()
-            if _G.X then
-              if game.workspace:FindFirstChild("Asta [Boss]") then
-                EquipToolSele("SukunaV2")
-                TP(game.workspace:FindFirstChild("Asta [Boss]").WorldPivot)
-                task.wait(1)
-                   game:GetService("VirtualInputManager"):SendKeyEvent(true, 120, false, game)
-                   game:GetService("VirtualInputManager"):SendKeyEvent(false, 120, false, game)
+            if _gv.X then
+                    local backpackHasTool = hasTool(game:GetService("Players").LocalPlayer.Backpack)
+                    local characterHasTool = game:GetService("Players").LocalPlayer.Character and hasTool(game:GetService("Players").LocalPlayer.Character)
+                      if backpackHasTool or characterHasTool then
+                         if game.workspace:FindFirstChild("Asta [Boss]") then
+                           EquipToolSele("SukunaV2")
+                           TP(game.workspace:FindFirstChild("Asta [Boss]").WorldPivot)
+                           task.wait(1)
+                           game:GetService("VirtualInputManager"):SendKeyEvent(true, 120, false, game)
+                           game:GetService("VirtualInputManager"):SendKeyEvent(false, 120, false, game)
+                         end
+                      else
+                      TP(game.workspace:FindFirstChild("Asta [Boss]").WorldPivot)
+                    for _, v in pairs(workspace:GetChildren()) do
+                        if v:IsA("Model") and v.Name == "Asta [Boss]" then
+                            local humanoid = v:FindFirstChild("Humanoid")
+                            local hrp = v:FindFirstChild("HumanoidRootPart")
+                            if hrp and humanoid and humanoid.Health > 0 then
+                                hrp.Size = Vector3.new(10, 30, 10)
+                                hrp.Transparency = 0.9
+                                hrp.CanCollide = false
+                                v.Head.CanCollide = false
+                                humanoid.WalkSpeed = 0
+                                humanoid.JumpPower = 0
+                                repeat
+                                    task.wait()
+                                    _Attack()
+                                    EquipTool()
+                                    if _gv.AFemtoV2 and humanoid.Health > 1 then
+                                        TP(hrp.CFrame * CFrame.new(0, 7, 0) * CFrame.Angles(math.rad(-90), 0, 0))
+                                    end
+                                until not _gv.AFemtoV2 or humanoid.Health <= 0
+                            end
+                        end
+                    end
+                end
+            end
+        end)
+    end
+end)
+spawn(function() 
+    while true do 
+        task.wait() 
+        pcall(function() 
+            if _gv.AFemto then 
+                if workspace:FindFirstChild("Femto [Boss]") then
+                    local backpackHasTool = hasTool(game:GetService("Players").LocalPlayer.Backpack) 
+                    local characterHasTool = game:GetService("Players").LocalPlayer.Character and hasTool(game:GetService("Players").LocalPlayer.Character) 
+                    if backpackHasTool or characterHasTool then 
+                        EquipToolSele("SukunaV2") 
+                        TP(workspace:FindFirstChild("Femto [Boss]").WorldPivot) 
+                        task.wait(1) 
+                        game:GetService("VirtualInputManager"):SendKeyEvent(true, 120, false, game) 
+                        game:GetService("VirtualInputManager"):SendKeyEvent(false, 120, false, game) 
+                    else 
+                        for _, v in pairs(workspace:GetChildren()) do 
+                            if v:IsA("Model") and v.Name == "Femto [Boss]" then 
+                                local humanoid = v:FindFirstChild("Humanoid") 
+                                local hrp = v:FindFirstChild("HumanoidRootPart") 
+                                if hrp and humanoid and humanoid.Health > 0 then 
+                                    hrp.Size = Vector3.new(10, 30, 10) 
+                                    hrp.Transparency = 0.9 
+                                    hrp.CanCollide = false 
+                                    v.Head.CanCollide = false 
+                                    humanoid.WalkSpeed = 0 
+                                    humanoid.JumpPower = 0 
+                                    repeat 
+                                        task.wait() 
+                                        _Attack() 
+                                        EquipTool() 
+                                        if _gv.AFemto and humanoid.Health > 1 then 
+                                            TP(hrp.CFrame * CFrame.new(0, 7, 0) * CFrame.Angles(math.rad(-90), 0, 0)) 
+                                        end 
+                                    until not _gv.AFemto or humanoid.Health <= 0 
+                                end 
+                            end 
+                        end 
+                    end 
+                else 
+                    local backpack = game:GetService("Players").LocalPlayer.Backpack 
+                    if backpack:FindFirstChild("Orb Dark") then 
+                        TP(workspace.Map["Spawn Boss"].Guts.WorldPivot) 
+                        fpp() 
+                    else 
+                        sendNotification("Rimuru Hub", "You don't have Orb Dark", 5) 
+                    end 
+                end 
+            end 
+        end) 
+    end 
+end)
+spawn(function()
+    while true do
+        task.wait()
+        pcall(function()
+            if _gv.AFemtoV2 then
+                if workspace:FindFirstChild("Femto [Boss]") then
+                    local backpackHasTool = hasTool(game:GetService("Players").LocalPlayer.Backpack)
+                    local characterHasTool = game:GetService("Players").LocalPlayer.Character and hasTool(game:GetService("Players").LocalPlayer.Character)
+                    if backpackHasTool or characterHasTool then
+                        EquipToolSele("SukunaV2")
+                        TP(game.workspace:FindFirstChild("Femto [Boss]").WorldPivot)
+                        task.wait(1)
+                        game:GetService("VirtualInputManager"):SendKeyEvent(true, 120, false, game)
+                        game:GetService("VirtualInputManager"):SendKeyEvent(false, 120, false, game)
+                    else
+                        for _, v in pairs(workspace:GetChildren()) do
+                            if v:IsA("Model") and v.Name == "Femto [Boss]" then
+                                local humanoid = v:FindFirstChild("Humanoid")
+                                local hrp = v:FindFirstChild("HumanoidRootPart")
+                                if hrp and humanoid and humanoid.Health > 0 then
+                                    hrp.Size = Vector3.new(10, 30, 10)
+                                    hrp.Transparency = 0.9
+                                    hrp.CanCollide = false
+                                    v.Head.CanCollide = false
+                                    humanoid.WalkSpeed = 0
+                                    humanoid.JumpPower = 0
+                                    repeat
+                                        task.wait()
+                                        _Attack()
+                                        EquipTool()
+                                        if _gv.AFemtoV2 and humanoid.Health > 1 then
+                                            TP(hrp.CFrame * CFrame.new(0, 7, 0) * CFrame.Angles(math.rad(-90), 0, 0))
+                                        end
+                                    until not _gv.AFemtoV2 or humanoid.Health <= 0
+                                end
+                            end
+                        end
+                    end
+                else
+                    local backpack = game:GetService("Players").LocalPlayer.Backpack
+                    if backpack:FindFirstChild("Orb Dark") then
+                        TP(workspace.Map["Spawn Boss"].Guts.WorldPivot)
+                        fpp()
+                    else
+                        local backpackHasTool = hasTool(game:GetService("Players").LocalPlayer.Backpack)
+                        local characterHasTool = game:GetService("Players").LocalPlayer.Character and hasTool(game:GetService("Players").LocalPlayer.Character)
+                        if backpackHasTool or characterHasTool then
+                            if game.workspace:FindFirstChild("Asta [Boss]") then
+                                EquipToolSele("SukunaV2")
+                                TP(game.workspace:FindFirstChild("Asta [Boss]").WorldPivot)
+                                task.wait(1)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, 120, false, game)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(false, 120, false, game)
+                            end
+                        else
+                            TP(game.workspace:FindFirstChild("Asta [Boss]").WorldPivot)
+                            for _, v in pairs(workspace:GetChildren()) do
+                                if v:IsA("Model") and v.Name == "Asta [Boss]" then
+                                    local humanoid = v:FindFirstChild("Humanoid")
+                                    local hrp = v:FindFirstChild("HumanoidRootPart")
+                                    if hrp and humanoid and humanoid.Health > 0 then
+                                        hrp.Size = Vector3.new(10, 30, 10)
+                                        hrp.Transparency = 0.9
+                                        hrp.CanCollide = false
+                                        v.Head.CanCollide = false
+                                        humanoid.WalkSpeed = 0
+                                        humanoid.JumpPower = 0
+                                        repeat
+                                            task.wait()
+                                            _Attack()
+                                            EquipTool()
+                                            if _gv.AFemtoV2 and humanoid.Health > 1 then
+                                                TP(hrp.CFrame * CFrame.new(0, 7, 0) * CFrame.Angles(math.rad(-90), 0, 0))
+                                            end
+                                        until not _gv.AFemtoV2 or humanoid.Health <= 0
+                                    end
+                                end
+                            end
+                        end
+                    end
                 end
             end
         end)
@@ -327,7 +525,7 @@ spawn(function()
         task.wait()
         pcall(function()
             if _gv.RKruba then
-               TP(CFrame.new(8.51999378, 4.49939966, 2362.82715, -0.989910245, 0, 0.141695946, 0, 1, -0, -0.141695946, 0, -0.989910245))
+               TP(workspace.Map.Shop["KruBa [250K Money]"].WorldPivot)
                   for _, v in pairs(game.workspace.Map.Shop:GetDescendants()) do
                     if v:FindFirstChild("ProximityPrompt") then
                      fireproximityprompt(v.ProximityPrompt, 0)
